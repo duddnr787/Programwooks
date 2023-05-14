730405f5ddefe0e2a270d9586ba1e166fe5ff6c0
YoungWook Kim <108661426+duddnr787@users.noreply.github.com>
2023-05-14 18:11:38 +0900

    [level 0] Title: 두 수의 차, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120803. 두 수의 차/두 수의 차.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    int a = num1;
+    int b = num2;
+    answer = a - b;
+    return answer;
+}
