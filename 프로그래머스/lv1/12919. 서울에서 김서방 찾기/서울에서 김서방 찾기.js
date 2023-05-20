505aa61a6d19e167a4970f17f5c9a04b4ce92de4
YoungWook Kim <108661426+duddnr787@users.noreply.github.com>
2023-05-20 15:30:29 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.07 ms, Memory: 32.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -0,0 +1,9 @@
+function solution(seoul) {
+    var answer = '';
+    seoul.map((i,a)=>{
+        if(seoul[i] = 'Kim'){
+            answer = "김서방은 " + a + "에 있다"
+        }
+    })
+    return answer;
+}
