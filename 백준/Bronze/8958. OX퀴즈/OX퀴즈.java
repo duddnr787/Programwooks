b8658ce1f4342b9fcee61894ca94a060fac0d2f0
YoungWook Kim <108661426+duddnr787@users.noreply.github.com>
2023-05-24 19:37:00 +0900

    [Bronze II] Title: OX퀴즈, Time: 224 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		String[] arr = new String[num];
+		
+		for(int i =0; i<arr.length; i++) {
+			arr[i] = sc.next();
+		}
+		for(int i =0; i<arr.length; i++) {
+		
+			int cnt = 0;
+			int sum = 0;
+			
+			for(int j = 0; j<arr[i].length(); j++) {
+				if(arr[i].charAt(j) == 'O') {
+					cnt++;
+				} else {
+					cnt = 0;
+				}
+				sum += cnt;
+			}
+			System.out.println(sum);
+		}
+		
+	}
+
+}
