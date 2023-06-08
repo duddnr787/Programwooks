0025b3da040998225fb111c5d591e4e833682678
YoungWook Kim <108661426+duddnr787@users.noreply.github.com>
2023-06-08 23:20:02 +0900

    [level 1] Title: 내적, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,14 +1,10 @@
 function solution(a, b) {
     var answer = 0;
-    if(a.length > b.length){
+    
         for(let i = 0; i<a.length; i++){
           answer += a[i] * b[i];
         } 
-    } else {
-        for(let i = 0; i<b.length; i++){
-          answer += a[i] * b[i];
-        } 
-    }
+
     
     return answer;
 }
