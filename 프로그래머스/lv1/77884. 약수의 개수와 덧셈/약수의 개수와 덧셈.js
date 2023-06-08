a9529901a7543e3d10c1fb0ec171cbfb52870ef2
YoungWook Kim <108661426+duddnr787@users.noreply.github.com>
2023-06-08 23:28:39 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.57 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,23 +1,24 @@
 function division(a){
     let count = 0;
     
-    for(let i = 1; i <= a; i++){
-        if( a % i === 0 ) {
-            count++;
+    for(let i = 1; i<=a; i++){
+        if(a % i == 0){
+            count ++;
         }
     }
+    
     return count;
 }
 
 function solution(left, right) {
     let answer = 0;
     
-    for(let i = left; i <= right; i++){
-        let count = division(i);
-        if(count % 2 === 0){
-            answer += i
+    for(let i = left; i<=right; i++){
+        let sum = division(i);
+        if(sum % 2 == 0){
+            answer += i;
         } else {
-            answer -= i
+            answer -= i;
         }
     }
     
