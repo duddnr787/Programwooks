df1c7e87f3e6debdd34466831f560bae29a409cb
YoungWook Kim <108661426+duddnr787@users.noreply.github.com>
2023-10-12 15:49:19 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT BOARD_ID, WRITER_ID, TITLE, PRICE, 
+case when status = 'SALE' then '판매중'
+     when status = 'DONE' then '거래완료'
+     when status = 'RESERVED' then '예약중' END STATUS
+FROM USED_GOODS_BOARD
+WHERE CREATED_DATE = '2022-10-05'
+ORDER BY BOARD_ID DESC
